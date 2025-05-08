28d2c48c6486e527c23434b6820d6a4b650f24dc
shg-ui <stangao2@illinois.edu>
2025-05-07 20:49:02 -0700

    Update validate.py

--- cxr_fairness/data/preprocess/validate.py
+++ cxr_fairness/data/preprocess/validate.py
@@ -5,9 +5,7 @@ def validate_mimic():
     img_dir = Path(Constants.image_paths['MIMIC'])
     assert (img_dir/'mimic-cxr-2.0.0-metadata.csv.gz').is_file()
     assert (img_dir/'mimic-cxr-2.0.0-negbio.csv.gz').is_file()
-    assert (img_dir/'mimic-cxr-2.0.0-negbio.csv.gz').is_file()
     assert (img_dir/'patients.csv.gz').is_file()    
-    assert (img_dir/'files/p19/p19316207/s55102753/31ec769b-463d6f30-a56a7e09-76716ec1-91ad34b6.jpg').is_file()
 
 def validate_cxp():
     img_dir = Path(Constants.image_paths['CXP'])
